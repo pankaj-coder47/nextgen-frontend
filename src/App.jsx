0431013240b36501ab4f1725bb503f95ac4fbfc4
dhanji-kumar <kdhanji183@gmail.com>
2023-12-27 18:36:38 +0530

    navbar responsive work half completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Navbar from './components/Navbar'
+import './app.css'
+
+export default function App() {
+  return (
+    <div className='appClass'>
+      <Navbar/>
+      <div className="hello"></div>
+    </div>
+  )
+}
